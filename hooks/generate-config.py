87330a836bb929ae003172b6cb5d7f29a0cfc65d
Ryan Hass <rhass@users.noreply.github.com>
2023-10-03 14:00:39 -0700

    Move serialized data constructor out of file write.
    This didn't really need to be in the file handler since it's just
    updating a variable with the appropriate data... so I decided to move
    this block.

--- hooks/generate-config.py
+++ hooks/generate-config.py
@@ -11,7 +11,6 @@ log.basicConfig(level=log.INFO, stream=sys.stderr)
 config_file_path = sys.argv[1] if len(sys.argv) > 1 else path.join(path.expanduser('~'), '.docker', 'config.json')  
 config = {}
 
-
 if path.exists(config_file_path):
     try:
         with open(config_file_path, 'r') as f:
@@ -22,17 +21,18 @@ if path.exists(config_file_path):
 
 log.info(f'Adding container registry to Docker config: %s' % environ['HOSTNAME'])
 
-with open(config_file_path, 'w') as f:
-    if len(config) == 0:
-        config = {
-            'auths': {
-                environ['HOSTNAME']: {
-                    'auth': base64.b64encode(f"{environ['DOCKER_USERNAME']}:{environ['DOCKER_PASSWORD']}".encode('utf-8')).decode('utf-8')
-                }
+if len(config) == 0:
+    config = {
+        'auths': {
+            environ['HOSTNAME']: {
+                'auth': base64.b64encode(f"{environ['DOCKER_USERNAME']}:{environ['DOCKER_PASSWORD']}".encode('utf-8')).decode('utf-8')
             }
         }
-    else:
-        config['auths'][environ['HOSTNAME']] = {
-            'auth': base64.b64encode(f"{environ['DOCKER_USERNAME']}:{environ['DOCKER_PASSWORD']}".encode('utf-8')).decode('utf-8')
-        }
+    }
+else:
+    config['auths'][environ['HOSTNAME']] = {
+        'auth': base64.b64encode(f"{environ['DOCKER_USERNAME']}:{environ['DOCKER_PASSWORD']}".encode('utf-8')).decode('utf-8')
+    }
+
+with open(config_file_path, 'w') as f:
     f.write(json.dumps(config, indent=4))
